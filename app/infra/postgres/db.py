b4d67277a7daed1aa7669e3f77dcd84401c2ab76
jGYQrw gga <Kul.vova2009@yandex.ru>
2025-09-08 03:51:41 +0300

    make a directory infra

---
+++ app/infra/postgres/db.py
@@ -0,0 +1,27 @@
+from pydantic import SecretStr, PostgresDsn, Secret
+import asyncpg
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
+class Database:
+    def __init__(self, dsn: Secret[PostgresDsn]):
+        self._dsn = dsn
+        self._pool: asyncpg.Pool | None = None
+        
+    async def initialize(self) -> None:
+        self._pool = await asyncpg.create_pool(str(self._dsn.get_secret_value()))
+        
+        
+    async def shutdown(self) -> None:
+        if self._pool is None:
+            raise Exception("Pool has not been created")
+        await self._pool.close()       
+        
+        
+    @asynccontextmanager
+    async def connection(self) -> AsyncGenerator[asyncpg.pool.PoolConnectionProxy, None]:
+        if self._pool is None:
+            raise Exception("Pool has not been created")
+        
+        async with self._pool.acquire() as connection:
+            yield connection
--- settings/config.py
+++ settings/config.py
@@ -1,6 +1,6 @@
 import os
 
-from pydantic import SecretStr
+from pydantic import SecretStr, PostgresDsn, Secret
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -10,3 +10,5 @@ class AppSettings(BaseSettings):
     )
     TELEGRAM_API_KEY: SecretStr = SecretStr("secret")
     LOG_LEVEL: str = "INFO"
+    POSTGRES_DSN: Secret[PostgresDsn] = Secret(
+        PostgresDsn("postgresql://postgres:Admin1994@localhost:5432/postgres"))
