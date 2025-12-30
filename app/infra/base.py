a2dbbcca934b28c916053f133acc89d11416394c
jGYQrw gga <Kul.vova2009@yandex.ru>
2025-12-30 03:13:27 +0300

    change file db.py

---
+++ app/infra/base.py
@@ -0,0 +1,6 @@
+from sqlalchemy.orm import DeclarativeBase
+
+
+# Создаем родительский класс
+class Base(DeclarativeBase):
+    pass
--- app/infra/postgres/db.py
+++ app/infra/postgres/db.py
@@ -1,26 +1,34 @@
-from pydantic import PostgresDsn, Secret
-import asyncpg
 from contextlib import asynccontextmanager
 from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import (create_async_engine,
+                                    async_sessionmaker,
+                                    AsyncSession)
+from sqlalchemy.orm import DeclarativeBase
+from pydantic import Secret, PostgresDsn
 
 
 class Database:
-    def __init__(self, dsn: Secret[PostgresDsn]):
-        self._dsn = dsn
-        self._pool: asyncpg.Pool | None = None
+    def __init__(self, dsn: Secret[PostgresDsn],
+                 declarative_base: type[DeclarativeBase]):
+        self._engine = create_async_engine(str(dsn.get_secret_value()))
+        self._async_session = async_sessionmaker(self._engine)
 
-    async def initialize(self) -> None:
-        self._pool = await asyncpg.create_pool(str(self._dsn.get_secret_value()))
+        self._declarative_base = declarative_base
 
     async def shutdown(self) -> None:
-        if self._pool is None:
-            raise Exception("Pool has not been created")
-        await self._pool.close()
+        await self._engine.dispose()
+
+    async def create_tables(self) -> None:
+        async with self._engine.begin() as conn:
+            await conn.run_sync(self._declarative_base.metadata.create_all)
 
     @asynccontextmanager
-    async def connection(self) -> AsyncGenerator[asyncpg.pool.PoolConnectionProxy, None]:
-        if self._pool is None:
-            raise Exception("Pool has not been created")
+    async def session(self) -> AsyncGenerator[AsyncSession, None]:
+        session: AsyncSession = self._async_session
 
-        async with self._pool.acquire() as connection:
-            yield connection
+        async with session:
+            try:
+                yield session
+            except Exception:
+                await session.rollback()
+                raise
